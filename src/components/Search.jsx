d9c9a88a9458dbfe97e4be061595af4a71f5bba7
6362prajju <150534382+6362prajju@users.noreply.github.com>
2025-08-27 18:17:20 +0530

    Add files via upload

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import find from '../assets/search.svg';
+
+const Search = ({searchTerm,setSearchTerm}) => {
+
+  return (
+    <div className="search" >
+      <div>
+        <img src={find} alt="search" />
+        <input 
+          type = "text"
+          placeholder='Search through thousands of movies'
+          value = {searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search
